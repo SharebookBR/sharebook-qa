8ffcced1d17f7f578e7307dd9c7e3edcd1fe9e4d
walter-lopes <walter_vinicius@outlook.com>
2018-08-25 20:57:11 -0300

    First test and basic project structure done.

---
+++ SharebookQA.UI/PageObjects/CommonPage.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using SharebookQA.UI.Helpers;
+using Xunit;
+
+namespace SharebookQA.UI.PageObjects
+{
+    public class CommonPage
+    {
+        private IWebDriver _driver;
+
+        public CommonPage(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        private IWebElement WelcomeUserID
+        {
+            get
+            {
+                return _driver.FindElement(By.Id("welcomeUser"));
+            }
+        }
+
+        public void CheckText(string text)
+        {
+            var innerText = WelcomeUserID.GetInnerText();
+            Assert.Equal(text, innerText);
+        }
+    }
+}
---
+++ SharebookQA.UI/PageObjects/LoginPage.cs
@@ -0,0 +1,45 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using SharebookQA.UI.Helpers;
+
+namespace SharebookQA.UI.PageObject
+{
+    public class LoginPage
+    {
+        private IWebDriver _driver;
+        public LoginPage(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        private IWebElement TxtEmail {
+            get
+            {
+                return _driver.FindElement(By.Id("txtEmail"));
+            }
+        }
+
+        private IWebElement TxtPassword
+        {
+            get
+            {
+                return _driver.FindElement(By.Id("txtPassword"));
+            }
+        }
+
+        private IWebElement BtnLogin
+        {
+            get
+            {
+                return _driver.FindElement(By.Id("btnLogin"));
+            }
+        }
+
+        public void Login(string email, string password)
+        {
+            TxtEmail.EnterText(email);
+            TxtPassword.EnterText(password);
+            BtnLogin.Click();   
+        }
+    }
+}
